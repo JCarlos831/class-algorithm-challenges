aa67c73bdbe239e868992fbbe5ae5c870b4e91da
Juan Carlos Montoya <jcmontoya@gmail.com>
2019-01-05 10:55:02 -0500

    Algorithm from 1/3/19 and 1/5/19

---
+++ FirstNonRepeatingCharacter/FirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,44 @@
+﻿// Given a string provide the first non repeating character
+// if no repeating character provide an underscore
+
+using System;
+using System.Collections.Generic;
+
+namespace FirstNonRepeatingCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindFirstNonRepeatingChar("abab");
+        }
+
+        private static void FindFirstNonRepeatingChar(string str)
+        {
+            var uniques = new List<char>();
+            while (str.Length > 0)
+            {
+                var count = 0;
+                foreach (var letter in str)
+                {
+                    if (str[0] == letter)
+                    {
+                        count++;
+                    }
+                }                
+                if (count == 1)
+                    uniques.Add(str[0]);
+
+                str = str.Replace(str[0].ToString(), string.Empty);
+            }
+
+            if (uniques.Count > 0)
+            {
+                Console.WriteLine(uniques[0]);
+                return;
+            }
+
+            Console.WriteLine('_');
+        }
+    }
+}
---
+++ ThisAndThat/ThisAndThat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ThisAndThat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod("rthids", "urejdn");
+        }
+
+        private static void NewMethod(string str1, string str2)
+        {
+            str1.ToCharArray();
+            str2.ToCharArray();
+
+            int[] difference = new int[str1.Length];
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                difference[i] = str1[i] - str2[i];
+            }
+
+            int sum = 0;
+            
+            foreach (var VARIABLE in difference)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+
+            foreach (var VARIABLE in difference)
+            {
+                sum += VARIABLE;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
