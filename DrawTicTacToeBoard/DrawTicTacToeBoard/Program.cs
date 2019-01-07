cca9e82398da1a2cb0e4cf3fd7355510c1ed3c0e
Juan Carlos Montoya <jcmontoya@gmail.com>
2019-01-07 18:50:07 -0500

    Dynamically sizable tic tac toe board

---
+++ DrawTicTacToeBoard/DrawTicTacToeBoard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DrawTicTacToeBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawBoard(10);
+        }
+
+        private static void DrawBoard(int size)
+        {
+            for (var i = 0; i < size-1; i++)
+            {
+                for (var j = 0; j < size-1; j++)
+                {
+                    Console.Write("   |   ");
+                }
+
+                Console.WriteLine();
+                for (var k = 0; k < size-1; k++)
+                {
+                    Console.Write("---+---");
+                }
+
+                Console.WriteLine();
+                for (var l = 0; l < size-1; l++)
+                {
+                    Console.Write("   |   ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
